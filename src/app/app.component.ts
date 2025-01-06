3b531f9878bea1dfebf65785dd838b2da178c13a
Nithyasri-2516 <nithyasri1625@gmail.com>
2025-01-06 15:49:02 +0530

    add drag-drop functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,82 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'simple-drag-drop-06';
+[x: string]: any;
+currentItem:any;
+ticketsArray: any[]=[
+    {
+      ticketId:'001',
+      ticketName:'layout1',
+      status:'In Progress',
+
+    },
+
+    {
+      ticketId:'101',
+      ticketName:'eg',
+      status:'To Do',
+
+    },
+    {
+      ticketId:'102',
+      ticketName:'eg2',
+      status:'To Do',
+
+    },
+
+    {
+      ticketId:'002',
+      ticketName:'layout2',
+      status:'Todo',
+
+    },
+
+    {
+      ticketId:'003',
+      ticketName:'layout3',
+      status:'In Progress',
+    },
+
+    {
+      ticketId:'004',
+      ticketName:'layout4',
+      status:'Done',
+    },
+
+    {
+      ticketId:'005',
+      ticketName:'layout5',
+      status:'Done',
+    },
+
+    
+    {
+      ticketId:'006',
+      ticketName:'layout6',
+      status:'To Do',
+    },
+  ];
+
+  filterTickkets(status: string){
+     return this.ticketsArray.filter(m=>m.status== status);
+  }
+  onDragStart(item:any){
+    console.log('start')
+     this.currentItem=item;
+  }
+
+  onDrop(event:any,status:string){
+    console.log('onDrop')
+    event.preventDefault();
+    const record=this.ticketsArray.find(m=>m.ticketId==this.currentItem.ticketId);
+    if(record!=undefined){
+      record.status=status;
+    }
+    this.currentItem=null;
+  }
+
+  onDragOver(event:any){
+     console.log('onDragOver')
+     event.preventDefault();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,12 +8,16 @@ import { AppComponent } from './app.component';
 @NgModule({
   declarations: [
     AppComponent
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+export { AppComponent };
+
